d016cb1f1e65ded9643b9ff029a217601970b555
Jack <jack37feus@hotmail.com>
2023-01-12 15:31:37 +0200

    added existing cpi work

---
+++ CPI_DataCLean_ForLoop.R
@@ -0,0 +1,33 @@
+rm(list = ls())
+
+setwd('C:/Users/RPHC5/Documents/R Scripts/CPI')
+
+library('tidyverse')
+library('readxl')
+
+Sources = c("Rural", "Urban", "All Rwanda")
+datalist = list()
+datalist = vector("list", length = length(Sources))
+
+
+
+for (i in Sources) {
+  data <- read_excel("CPI_time_series_November_2022.xls", sheet=i, skip=3, col_names=TRUE)%>%
+    slice(-1,-20)%>%
+    rename(Province = "...1",
+           U_R = "...2",
+           COICOP = "...3",
+           Products = "...4")%>%
+    pivot_longer(!c(Province, U_R, COICOP, Products, Weights), names_to = "Date", values_to = "Index")%>%
+    mutate(Date = as.Date(as.numeric(Date), origin = "1899-12-30"),
+           Products = gsub("v", "", Products),
+           Products = str_squish(Products),
+           Source = i)
+  datalist[[i]] <- data
+}
+
+combined = do.call(rbind, datalist)
+
+write.csv(combined,'november.csv')
+
+data%>%filter(Products=="GENERAL INDEX (CPI)")
